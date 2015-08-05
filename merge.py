468596b2442fdcdd1bd93693e3eab1520023c4d7
Jim Martens <github@2martens.de>
2015-08-05 17:11:17 +0200

    Added PDF merge functionality

---
+++ merge.py
@@ -0,0 +1,13 @@
+"""merge.py: Provides functionality to merge PDF files"""
+
+__author__ = 'Jim Martens'
+
+from PyPDF2 import PdfFileReader, PdfFileMerger
+
+
+def merge_pdf_files(name, files):
+    merger = PdfFileMerger()
+    for file in files:
+        merger.append(PdfFileReader(file['path'], 'rb'))
+
+    merger.write(name)
