9ebf974193c678ec01668d65c96ea2a76564e395
Jim Martens <github@2martens.de>
2015-09-03 15:04:17 +0200

    Removed obsolete parameter

--- merge.py
+++ merge.py
@@ -8,6 +8,6 @@ from pypdf2.PyPDF2 import PdfFileReader, PdfFileMerger
 def merge_pdf_files(name, files):
     merger = PdfFileMerger()
     for file in files:
-        merger.append(PdfFileReader(file['path'], 'rb'))
+        merger.append(PdfFileReader(file['path']))
 
     merger.write(name)
