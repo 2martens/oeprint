48ad639e1c8f547275c28faad07d6c61ebb17e15
Jim Martens <github@2martens.de>
2016-08-29 12:36:34 +0200

    [Client] Implemented save functionality in-memory
    Still missing:
    * persisting this change to the data.json file
    
    Signed-off-by: Jim Martens <github@2martens.de>

--- client/edit_view.py
+++ client/edit_view.py
@@ -9,7 +9,8 @@ from PyQt5.QtWidgets import QWidget
 
 from data import Configuration
 from data import DataStorage
-from helper.model_helper import create_new_tree_item, get_item, is_checked
+from data import Material
+from helper.model_helper import create_new_tree_item, get_item, is_checked, is_checked_tree, check_item
 
 
 class EditView(QDialog):
@@ -134,6 +135,7 @@ class EditView(QDialog):
         material_print_amounts = configuration.get_material_print_amounts()
         for material in configuration.get_materials():
             item = get_item(self._materialTreeWidget.invisibleRootItem(), material.get_name())
+            check_item(item)
             if item is not None:
                 print_amount = material_print_amounts[material.get_name()]
                 item.setText(1, str(print_amount))
@@ -141,6 +143,7 @@ class EditView(QDialog):
         configuration_print_amounts = configuration.get_config_print_amounts()
         for configuration in configuration.get_configurations():
             item = get_item(self._configurationTreeWidget.invisibleRootItem(), configuration.get_name())
+            check_item(item)
             if item is not None:
                 print_amount = configuration_print_amounts[configuration.get_name()]
                 item.setText(1, str(print_amount))
@@ -148,8 +151,58 @@ class EditView(QDialog):
     @staticmethod
     def _add_configuration(configuration, item):
         create_new_tree_item(configuration.get_name(), item)
+        
+    def _calculate_print_amounts(self, root_item):
+        """
+        :param root_item:
+        :type root_item: QTreeWidgetItem
+        :rtype: dict
+        """
+        print_amounts = {}
+        if root_item.childCount() > 0:
+            for child_index in range(root_item.childCount()):
+                child = root_item.child(child_index)
+                if is_checked_tree(child):
+                    print_amounts[child.text(0)] = int(child.text(1))
+                else:
+                    child_print_amounts = self._calculate_print_amounts(child)
+                    print_amounts.update(child_print_amounts)
+
+        elif is_checked_tree(root_item):
+            print_amounts[root_item.text(0)] = int(root_item.text(1))
+
+        return print_amounts
     
     def _save(self):
+        # update sub configurations
+        config_print_amounts = self._calculate_print_amounts(self._configurationTreeWidget.invisibleRootItem())
+        for config in self._currentConfiguration.get_configurations():  # type: Configuration
+            if config.get_name() not in config_print_amounts:
+                self._currentConfiguration.remove_configuration(config)
+            else:
+                self._currentConfiguration.set_config_print_amount(config, config_print_amounts[config.get_name()])
+                del config_print_amounts[config.get_name()]
+
+        data = DataStorage()
+        for config_name in config_print_amounts:
+            config = data.get_configuration(config_name)
+            self._currentConfiguration.add_configuration(config, config_print_amounts[config_name])
+        
+        # update material configurations
+        material_print_amounts = self._calculate_print_amounts(self._materialTreeWidget.invisibleRootItem())
+        for material in self._currentConfiguration.get_materials():  # type: Material
+            if material.get_name() not in material_print_amounts:
+                self._currentConfiguration.remove_material(material)
+            else:
+                self._currentConfiguration.set_material_print_amount(
+                    material, material_print_amounts[material.get_name()]
+                )
+                del material_print_amounts[material.get_name()]
+        
+        for material_name in material_print_amounts:
+            material = data.get_material(material_name)
+            self._currentConfiguration.add_material(material, material_print_amounts[material_name])
+        
         self.done(QDialog.Accepted)
 
     def show_for_configuration(self, configuration):
